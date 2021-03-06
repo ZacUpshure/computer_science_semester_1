269474cff2c2c5d4961de350e5bdd2a40f8c7238
Zac <zac.upshure@outlook.com>
2021-03-06 11:26:00 +0100

    Update widerstandsrechner.c

--- widerstandsrechner.c
+++ widerstandsrechner.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-	double R1, R2, R3;
+	double R1, R2, R3; // decleration of variables
 	
 	printf("Geben sie ihre drei Widerstände ein\n");
 
 	printf("Widerstand 1 Eingeben");
-	fflush(stdout);
-	scanf("%lf", &R1);
+	fflush(stdout); // clears buffer.
+	scanf("%lf", &R1); // input function 'with &', 'no \n'!
 
 	printf("Widerstand 2 Eingeben");
 	fflush(stdout);
@@ -22,12 +22,12 @@ int main()
 	printf("\nIher Widerstaende :\n R1: %lf\n R2: %lf\n R3: %lf\n");
 
 	printf("Gesamtwiderstand 'Reihenschaltung' :");
-	double Rges1 = R1 + R2 + R3;
+	double Rges1 = R1 + R2 + R3; // addition
 	printf("%lf\n", Rges1);
 
 	printf("Gesamtwiderstand 'Parallelschaltung' :");
-	double Rges2 = 1.0/R1 + 1.0/R2 + 1.0/R3;
-	printf("%lf\n", Rges2);
+	double Rges2 = 1.0/R1 + 1.0/R2 + 1.0/R3; // division '1.0 instead of 1
+	printf("%lf\n", Rges2);			 // otherwise result = 0;
 
 	printf("Wheatstonesche Messbrücke : ");
 	double RU = R3 * R1 / R2;

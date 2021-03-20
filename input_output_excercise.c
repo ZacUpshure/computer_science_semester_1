f40a8d5b3ffcd009dcedb49e42dd817a76bcbe11
Zac <zac.upshure@outlook.com>
2021-03-20 18:14:41 +0100

    Update input_output_excercise.c

--- input_output_excercise.c
+++ input_output_excercise.c
@@ -5,59 +5,59 @@ int main()
 	int intiger;
 	char repeat;
 	char dummy;
-	char newline;
+	char newline;		//decleration
 
 	do
 	{
 		printf("\nPlease enter an intiger: \t");
 		
-		if(scanf("%d%c", &intiger, &newline) !=2 )
+		if(scanf("%d%c", &intiger, &newline) !=2 )	//asks for number
 		{
 			printf("wrong input.\n");
-			do
+			do					//--------------------
 			{
-				scanf("%c", &dummy);
+				scanf("%c", &dummy);		//clear keyboard buffer
 			}
-			while(dummy != '\n');
+			while(dummy != '\n');			//--------------------
 		}
 		else
 		{
-			printf("your input: %d\n", intiger);
+			printf("your input: %d\n", intiger);	//prints the number
 			if (newline != '\n')
 			{
-				do
+				do				//--------------------
 				{
-					scanf("%c", &dummy);
+					scanf("%c", &dummy);	//clear keyboard buffer
 				}
-				while(dummy != '\n');
+				while(dummy != '\n');		//--------------------
 			}
 		}
 
-		printf("\nrepeat ?\t");
+		printf("\nrepeat ?\t");				//asking for repetition
 		scanf("%c", &dummy);
-		do
+		do						//--------------------
 		{
-			scanf("%c", &dummy);
+			scanf("%c", &dummy);			//clear keyboard buffer
 		}
-		while(dummy != '\n');
+		while(dummy != '\n');				//--------------------
 
-		if(repeat != 'Y' && repeat != 'y' && repeat != 'N' && repeat != 'n')
+		if(repeat != 'Y' && repeat != 'y' && repeat != 'N' && repeat != 'n') //asks again if the user makes an invalid input
 		{
-			printf("\ntype (y/n) :\t");
+			printf("\ntype (y/n) :\t");		//asking for input
 			scanf("%c", &repeat);
-			do
+			do					//--------------------
 			{
-				scanf("%c", &dummy);
+				scanf("%c", &dummy);		//clear keyboard buffer
 			}
-			while(dummy != '\n');
+			while(dummy != '\n');			//--------------------
 		}
-		else if(repeat == 'n' || repeat == 'N')
+		else if(repeat == 'n' || repeat == 'N')		//shuts down program
 		{
 			printf("programm shut down.");
 			return 0;
 		}
-	}
-	while(repeat == 'y' || repeat == 'Y');
+	}	
+	while(repeat == 'y' || repeat == 'Y');			//repeats the program
 
 	return 0;
 }

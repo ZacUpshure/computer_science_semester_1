2cb793db74def251ae74080bd170b4acbab4b4a6
Zac <zac.upshure@outlook.com>
2021-07-11 12:12:25 +0200

    Delete Aufgabe_5 directory

--- Aufgabe_5/menu (2).h
+++
@@ -1,7 +0,0 @@
-#ifndef MENU_H_INCLUDED
-#define MENU_H_INCLUDED
-
-int getMenu(char const *title, char const **aMenuPoints, unsigned short numMenuPoints);
-
-
-#endif // MENU_H_INCLUDED
--- Aufgabe_5/sort.h
+++
@@ -1,7 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-#include "datastructure.h"
-
-void QuickSort(int(*cmpfct)(sPlayer *, sPlayer *));
-
-#endif
